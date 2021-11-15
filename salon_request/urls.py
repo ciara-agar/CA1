53bec581bdd01199828e5a545c6b99c4a91b220e
Ciara Agar <X00174339@mytudublin.ie>
2021-11-15 10:09:48 +0000

    error fixed, json file created

--- salon_request/urls.py
+++ salon_request/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import RequestListView, RequestDetailView, RequestCreateView, RequestUpdateView, RequestDeleteView
 
 urlpatterns = [
-    path('', RequestListView().as_view, name='request_list'),
+    path('', RequestListView.as_view(), name='request_list'),
     path('request/<int:pk>/', RequestDetailView.as_view(), name='request_detail'),
     path('request/new/', RequestCreateView.as_view(), name='request_new'),
     path('request/<int:pk>/edit/', RequestUpdateView.as_view(), name='request_edit'),
