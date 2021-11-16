34b3316ee27372a3c936a2740e184a2619ab8ef0
Ciara Agar <X00174339@mytudublin.ie>
2021-11-16 00:26:48 +0000

    error of duplicate app name

--- salon_client/apps.py
+++ salon_client/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SalonReviewConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'salon_review'
+    name = 'salon_reviews'
