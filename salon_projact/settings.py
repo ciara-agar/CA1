4d46d1d9df1330435ec6786c459d87922c89d6ea
Fionnuala Hyndman <x00179539@mytudublin.ie>
2021-11-16 10:32:29 +0000

    password change and reset added

--- salon_projact/settings.py
+++ salon_projact/settings.py
@@ -148,3 +148,4 @@ MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
